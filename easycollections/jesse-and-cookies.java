ea17aa800db9423651766be081e5e2daea916904
sadhanala lakshmi manikanta <113621583+mani1105@users.noreply.github.com>
2023-02-08 10:06:10 +0530

    Create jesse-and-cookies.java

---
+++ easycollections/jesse-and-cookies.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'cookies' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY A
+     */
+
+    public static int cookies(int k, List<Integer> A) {
+    // Write your code here
+        PriorityQueue<Integer> p = new PriorityQueue<Integer>();
+        int c1=0;
+        for(int i=0;i<A.size();i++){
+            int c=A.get(i);
+            p.add(c);
+        }//System.out.println(p);
+        while(p.peek() < k && p.size() >=2){
+            int a=p.poll()+2*p.poll();
+            p.add(a);
+            c1=c1+1;
+        }
+        if(p.peek()>k){
+            return c1;
+        }else{
+            return -1;
+        }
+        
+        
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] ATemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> A = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int AItem = Integer.parseInt(ATemp[i]);
+            A.add(AItem);
+        }
+
+        int result = Result.cookies(k, A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
